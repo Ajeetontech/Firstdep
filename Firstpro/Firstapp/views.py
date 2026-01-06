a4f9ad7f9927f27c5d8966d91e4a9ad282ea26e8
Nirbhay231 <144772839+Nirbhay231@users.noreply.github.com>
2026-01-06 08:00:52 +0530

    changes by ganesh

--- Firstpro/Firstapp/views.py
+++ Firstpro/Firstapp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def Index(request):
+	print('Hello')
     st='<h1>Welcome to our first Application</h1>'
     return HttpResponse(st)
